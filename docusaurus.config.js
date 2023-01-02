6fa78de9d3d18ffe379d55d8d6e7509edaea7bd2
dungdo <dungdo@osd.vn>
2023-01-02 18:00:59 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,7 +115,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} OSD.VN.`,
       },
       prism: {
         theme: lightCodeTheme,
